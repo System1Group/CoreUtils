a9b05e47facde506d4ca1a90cca8fb0b48a9234f
Meeji <sebastian.brabiner@gmail.com>
2016-12-01 15:59:36 +0000

    Added basic set of core utilities

---
+++ CoreUtils/LazyValue/StaticLazyValue.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreUtils
+{
+    using System;
+
+    public static class LazyValue
+    {
+        public static LazyValue<T> Create<T>(Func<T> factory)
+        {
+            return new LazyValue<T>(factory);
+        }
+    }
+}
